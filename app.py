a18f99298930b3284aa6cafc3a426b245677b429
BogdanIvashchenko <bogdan.ivashchenko@gmail.com>
2017-09-21 17:31:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    result = urlopen("https://api.telegram.org/bot" + bot_id + "/sendMessage", urlencode({ "chat_id": 383189785, "text": json.dumps(req, indent=4, ensure_ascii=False) }).encode("utf-8")).read()
+    result = urlopen("https://api.telegram.org/bot" + bot_id + "/sendMessage", urlencode({ "chat_id": -242861658, "text": json.dumps(req, indent=4, ensure_ascii=False) }).encode("utf-8")).read()
 
     res = makeWebhookResult(req)
 
